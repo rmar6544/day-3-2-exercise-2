ca405cf0f5a0082df3b569ebac794528892b6f4f
Romy M <rmar6544@gmail.com>
2021-04-01 19:36:10 +0000

    made code better

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 #writeing a bmi calculator  that will print if you are, underweight, overweight, or obese
-bmi_weight = float(weight)
-bmi_height = float(height)**2
-bmi = round(bmi_weight / bmi_height)
+bmi = round(weight / height **2)
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi > 18.5 and bmi < 25:
